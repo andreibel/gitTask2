2e4df867e4d9e45e66a0dd3a3ec77862a706dba6
halab <halab@10.10.5.195>
2025-11-05 10:34:01 +0200

    change  the apperance of the array to arrayList

--- test1/src/main/java/com/andreibel/TestThreadCheckArray.java
+++ test1/src/main/java/com/andreibel/TestThreadCheckArray.java
@@ -8,10 +8,12 @@ public class TestThreadCheckArray {
 			Thread thread1, thread2;
 			System.out.println("Enter array size");
 			int num  = input.nextInt();
-			int [] array = new int[num];
+			//int [] array = new int[num];
+			ArrayList<Integer> array = new ArrayList<Integer>();
 			System.out.println("Enter numbers for array");
 			
 			for (int index = 0; index < num; index++) 
+				array.add(input.nextInt());
 				array[index] = input.nextInt();
 			
 			System.out.println("Enter number");
@@ -37,9 +39,9 @@ public class TestThreadCheckArray {
 				System.out.println("Sorry");
 				return;
 			}
-			System.out.println("Solution for b : " + sd.getB() + ",n = " + sd.getArray().length);
+			System.out.println("Solution for b : " + sd.getB() + ",n = " + sd.getArray().size());
 			System.out.print("I:    ");
-			for(int index = 0; index < sd.getArray().length ; index++)
+			for(int index = 0; index < sd.getArray().size(); index++)
 				System.out.print(index + "    ");
 			System.out.println();
 			System.out.print("A:    ");
