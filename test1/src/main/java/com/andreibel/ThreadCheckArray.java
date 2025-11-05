6f36f39e2f448c641006eb0bc0fe9705ecbbdb49
andreibel <andreinw12@gmail.com>
2025-11-05 10:07:26 +0200

    add files of the code, change the array in threadCheckArray from array to ArrayList

---
+++ test1/src/main/java/com/andreibel/ThreadCheckArray.java
@@ -0,0 +1,85 @@
+package com.andreibel;
+
+import java.util.ArrayList;
+
+public class ThreadCheckArray implements Runnable
+{
+	private boolean flag;
+	private boolean [] winArray;
+	SharedData sd;
+	//int[] array;
+    ArrayList<Integer> array;
+	int b;
+	
+	public ThreadCheckArray(SharedData sd)
+    {
+		this.sd = sd;	
+		synchronized (sd) 
+		{
+			array = sd.getArray();
+			b = sd.getB();
+		}		
+		winArray = new boolean[array.size()];
+	}
+	
+	void rec(int n, int b)
+	{
+		synchronized (sd) 
+		{
+			if (sd.getFlag())
+				return;
+		}	
+		if (n == 1)
+		{
+			if(b == 0 || b == array.getFirst())
+			{
+				flag = true;
+				synchronized (sd) 
+				{
+					sd.setFlag(true);
+				}			
+			}
+			if (b == array.getFirst())
+				winArray[n-1] = true;
+			return;
+		}
+		
+		rec(n-1, b - array.get(n-1));
+		if (flag)
+			winArray[n-1] = true;
+		synchronized (sd) 
+		{
+			if (sd.getFlag())
+				return;
+		}	
+		rec(n-1, b);
+	}
+
+    @Override
+	public void run() {
+        if (array.size() != 1)
+			if (Thread.currentThread().getName().equals("thread1"))
+				rec(array.size() -1, b - array.getLast());
+			else 
+				rec(array.size() -1, b);
+		if (array.size() == 1)
+			if (b == array.getFirst() && !flag)
+			{
+				winArray[0] = true;
+				flag = true;
+				synchronized (sd) 
+				{
+					sd.setFlag(true);
+				}
+			}
+		if (flag)
+		{
+			if (Thread.currentThread().getName().equals("thread1"))
+				winArray[array.size() - 1] = true;
+			synchronized (sd) 
+			{
+				sd.setWinArray(winArray);
+			}	
+		}
+	}
+}
